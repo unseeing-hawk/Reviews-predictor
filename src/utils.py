f78ca1c45102c0817ed7152721781104903c354b
p.pankova <p.pankova@NB-3683.local>
2024-04-08 23:52:54 +0300

    fix read_data

--- src/utils.py
+++ src/utils.py
@@ -3,17 +3,18 @@ import numpy as np
 import pandas as pd
 
 
-path = r'resources/Data/'
+path = r'resourses/Data/'
 
 def read_data():
-    df = pd.DataFrame(columns=['reviews', 'sentiment'])
-    
+    df = pd.DataFrame(columns=['review', 'sentiment'])
+
     for directory in os.listdir(path):
-        if os.path.isdir(os.path.join(path, directory)):
-            files = np.array(os.listdir(os.path.join(path, directory)))
+        if os.path.isdir(path + directory):
+            files = np.array(os.listdir(path + directory))
             for file in files:
-                with open(os.path.join(path, directory, file), encoding='utf-8') as f:
+                with open(os.path.join(path + directory + '/', file), encoding='utf-8') as f:
                     review = f.read()
-                    current_df = pd.DataFrame({'reviews': [review], 'sentiment': directory})
+                    current_df = pd.DataFrame({'review': [review], 'sentiment': directory})
                     df = pd.concat([df, current_df], ignore_index=True)
+
     return df
